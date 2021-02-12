40191e4d194ef546098ab5635f96df6e37c7523f
Anvay <anvaymishra5@gmail.com>
2021-02-12 21:33:36 +0530

    implement basic drag and drop

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React ,{useState} from 'react';
 import './App.css';
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import _ from 'lodash';
+import {v4} from "uuid"
+
+
+const tile = {
+  id: v4(),
+  content: "Complete the project"
+}
+
+const tile2 = {
+  id: v4(),
+  content: "start the project"
+}
 
 function App() {
   const [state,setState] = useState({
     "todo": {
       title: "To-do",
-      tiles: []
+      tiles: [tile]
     },
     "inProgress": {
       title: "In Progress",
-      tiles: []
+      tiles: [tile2]
     },
     "completed": {
       title: "Completed",
@@ -23,6 +35,8 @@ function App() {
       <DragDropContext onDragEnd = {e => console.log(e)}>
       {_.map(state,(data,key)=> {
         return (
+          <div  key = {key} className= {"column"}>
+            <h3>{data.title}</h3>
           <Droppable droppableId = {key}>
             {(provided,snapshot)=>{
               return (
@@ -31,11 +45,28 @@ function App() {
                   {...provided.droppableProps}
                   className = {"droppable-column"}
                 >
-                  
+                  {data.tiles.map((ele :T, index :num)=>{
+                    return(
+                      <Draggable key = {ele.id} index = {index} draggableId = {ele.id}>
+                        {(provided) => {
+                          return (
+                            <div
+                              ref = {provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              >
+                                {ele.content}
+                            </div>
+                          )
+                        }}
+                      </Draggable>
+                    )
+                  })}
                 </div>
               )
             }}
           </Droppable>
+          </div>
         )
       })}
       </DragDropContext>
