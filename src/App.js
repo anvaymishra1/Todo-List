06d212164bb3a8c5af5331e365c080d5b6f3fc36
Anvay <anvaymishra5@gmail.com>
2021-02-12 21:06:25 +0530

    initialsed basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React ,{useState} from 'react';
 import './App.css';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import _ from 'lodash';
 
 function App() {
+  const [state,setState] = useState({
+    "todo": {
+      title: "To-do",
+      tiles: []
+    },
+    "inProgress": {
+      title: "In Progress",
+      tiles: []
+    },
+    "completed": {
+      title: "Completed",
+      tiles: []
+    }
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DragDropContext onDragEnd = {e => console.log(e)}>
+      {_.map(state,(data,key)=> {
+        return (
+          <Droppable droppableId = {key}>
+            {(provided,snapshot)=>{
+              return (
+                <div 
+                  ref = {provided.innerRef}
+                  {...provided.droppableProps}
+                  className = {"droppable-column"}
+                >
+                  
+                </div>
+              )
+            }}
+          </Droppable>
+        )
+      })}
+      </DragDropContext>
     </div>
   );
 }
